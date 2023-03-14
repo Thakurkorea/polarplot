4e07829b4c4381f0bd134bfe67e7ad05bb71e7bb
Thakurkorea <52812653+Thakurkorea@users.noreply.github.com>
2023-03-14 23:40:22 +0900

    Update Circular_dendo.R

--- Circular_dendo.R
+++ Circular_dendo.R
@@ -14,3 +14,14 @@ hc <- hclust(dist(rand_matrix), "ward.D2")
 
 # create a star tree dendrogram plot
 plot(as.phylo(hc), type = "fan", cex = 0.6, edge.width = 0.5, edge.col = "gray")
+## cluster fixing
+plot(hc)
+abline(h = mean(dist(rand_matrix[1:50,2:3])), col = 'red')
+abline(h = 1000, col = 'red')   ## nearest 0
+
+# color fixing 
+# Cut the dendrogram into 5 clusters
+colors = c("red", "blue", "green", "black",'magenta')
+clus4 = cutree(hc, 5)
+plot(as.phylo(hc), type = "fan", tip.color = colors[clus4],
+     label.offset = 1, cex = 0.7)
